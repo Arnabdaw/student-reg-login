02bf911c005e4f6bce5038afd78950bea14cebf3
Arnabdaw <arnabdaw1@gmail.com>
2025-06-19 15:30:52 +0530

    final

--- student_nic/student-reg-login-main/src/components/Login.jsx
+++ student_nic/student-reg-login-main/src/components/Login.jsx
@@ -27,7 +27,9 @@ const Login = ({ onTogglePage, onLoginSuccess }) => {
   }
     
     console.log('Login attempt with:', { email, password });
-    onLoginSuccess(); 
+    // onLoginSuccess(); 
+
+    
   };
 
   return (
